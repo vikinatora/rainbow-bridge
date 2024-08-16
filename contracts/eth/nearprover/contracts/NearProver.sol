cb0b5132778f33c98594ef2f413a08eda5b05cca
vikinatora <vikinatora@gmail.com>
2024-08-16 14:33:02 +0300

    wip: modify nearx to accomodate for l1 verication changes

--- contracts/eth/nearprover/contracts/NearProver.sol
+++ contracts/eth/nearprover/contracts/NearProver.sol
@@ -2,40 +2,41 @@
 pragma solidity ^0.8;
 
 import "rainbow-bridge-sol/nearbridge/contracts/AdminControlled.sol";
-import "rainbow-bridge-sol/nearbridge/contracts/INearBridge.sol";
+import "rainbow-bridge-sol/nearbridge/contracts/INearX.sol";
 import "rainbow-bridge-sol/nearbridge/contracts/NearDecoder.sol";
 import "./ProofDecoder.sol";
 import "./INearProver.sol";
+import "hardhat/console.sol";
 
 contract NearProver is INearProver, AdminControlled {
     using Borsh for Borsh.Data;
     using NearDecoder for Borsh.Data;
     using ProofDecoder for Borsh.Data;
 
-    INearBridge public bridge;
+    INearX public nearX;
 
     constructor(
-        INearBridge _bridge,
+        address _nearX,
         address _admin,
         uint _pausedFlags
     ) AdminControlled(_admin, _pausedFlags) {
-        bridge = _bridge;
+        nearX = INearX(_nearX);
     }
 
     uint constant UNPAUSE_ALL = 0;
     uint constant PAUSED_VERIFY = 1;
 
-    function proveOutcome(bytes memory proofData, uint64 blockHeight)
-        public
-        view
-        override
-        pausable(PAUSED_VERIFY)
-        returns (bool)
-    {
+    function proveOutcome(bytes memory proofData) public view returns (bool) {
         Borsh.Data memory borsh = Borsh.from(proofData);
         ProofDecoder.FullOutcomeProof memory fullOutcomeProof = borsh.decodeFullOutcomeProof();
-        borsh.done();
+        borsh.done();        
 
+        // Step 1. Verify the block header hash against the latestHeader
+        // The header hash is precalculated inside block_header_lite.hash,
+        // so we can just use it instead of recalculating.
+        require(fullOutcomeProof.block_header_lite.hash == nearX.latestHeader(), "NearProver: block header does not match latest header");
+
+        // Step 2: Verify the outcome proof within the block (unchanged)
         bytes32 hash = _computeRoot(
             fullOutcomeProof.outcome_proof.outcome_with_id.hash,
             fullOutcomeProof.outcome_proof.proof
@@ -50,8 +51,13 @@ contract NearProver is INearProver, AdminControlled {
             "NearProver: outcome merkle proof is not valid"
         );
 
-        bytes32 expectedBlockMerkleRoot = bridge.blockMerkleRoots(blockHeight);
+        // Step 3. Verify the block merkle root
+        // bytes32 expectedBlockMerkleRoot = bridge.blockMerkleRoots(blockHeight);
 
+        // We have verified the block header, so we can use the block merkle root 
+        // from the light client header instead of fetching it from the contract.
+        bytes32 expectedBlockMerkleRoot = fullOutcomeProof.block_header_lite.inner_lite.block_merkle_root;
+        
         require(
             _computeRoot(fullOutcomeProof.block_header_lite.hash, fullOutcomeProof.block_proof) ==
                 expectedBlockMerkleRoot,
--- contracts/eth/nearprover/test/NearProver.js
+++ contracts/eth/nearprover/test/NearProver.js
@@ -3,107 +3,49 @@ const { ethers } = require('hardhat');
 const { borshifyOutcomeProof } = require(`rainbow-bridge-utils`);
 const fs = require('fs').promises;
 const { computeMerkleRoot } = require('../utils/utils');
+const bs58 = require('bs58');
 
-let NearProver, NearBridgeMock;
+let NearProver, NearXMock;
 
 beforeEach(async function () {
-    NearBridgeMock = await (await ethers.getContractFactory('NearBridgeMock')).deploy();
+    NearXMock = await (await ethers.getContractFactory('NearXMock')).deploy();
     NearProver = await (await ethers.getContractFactory('NearProver')).deploy(
-        NearBridgeMock.address,
+        NearXMock.address,
         ethers.constants.AddressZero,
         0
     );
 });
 
-async function testProof(merkleRoot, height, proofPath) {
+async function testProof(proofPath) {
     let proof = require(proofPath);
-    console.log(computeMerkleRoot(proof).toString('hex'));
-    expect(merkleRoot === '0x' + computeMerkleRoot(proof).toString('hex')).to.be.true;
+    let blockHash = ethers.utils.hexlify(bs58.decode(proof.outcome_proof.block_hash));
+    // Set the latest header to match the proof's block hash
+    await NearXMock.setLatestHeader(blockHash);
+    // Borshify the outcome proof
     proof = borshifyOutcomeProof(proof);
-    await NearBridgeMock.setBlockMerkleRoot(height, merkleRoot);
-    expect(await NearProver.proveOutcome(proof, height)).to.be.true;
+
+    expect(await NearProver.proveOutcome(proof)).to.be.true;
 }
 
+
 it('should be ok', async function () {
-    await testProof('0x22f00dd154366d758cd3e4fe81c1caed8e0db6227fe4b2b52a8e5a468aa0a723', 498, './proof2.json');
-    await testProof('0x0d0776820a9a81481a559c36fd5d69c33718fb7d7fd3be7564a446e043e2cb35', 1705, './proof3.json');
-    await testProof('0x1f7129496c461c058fb3daf258d89bf7dacb4efad5742351f66098a00bb6fa53', 5563, './proof4.json');
-    await testProof('0xa9cd8eb4dd92ba5f2fef47d68e1d73ac8c57047959f6f8a2dcc664419e74e4b8', 384, './proof5.json');
-    await testProof('0xcc3954a51b7c1a86861df8809f79c2bf839741e3e380e28360b8b3970a5d90bd', 377, './proof6.json');
-    await testProof('0x8298c9cd1048df03e9ccefac4b022636a30a2f7e6a8c33cc4104901b92e08dfd', 93700915, './proof7.json');
+    await testProof('./proof2.json');
+    await testProof('./proof3.json');
+    await testProof('./proof4.json');
+    await testProof('./proof5.json');
+    await testProof('./proof6.json');
+    await testProof('./proof7.json');
 });
 
 if (process.env['NEAR_PROOFS_DIR']) {
-    it('should able to verify proofs from dump', async function () {
+    it('should be able to verify proofs from dump', async function () {
         this.timeout(0);
         let proofFiles = await fs.readdir(process.env['NEAR_PROOFS_DIR']);
 
         for (let i = 0; i < proofFiles.length; i++) {
-            let proof = require(process.env['NEAR_PROOFS_DIR'] + '/' + proofFiles[i]);
-            const height = proof.block_header_lite.inner_lite.height;
-            await NearBridgeMock.setBlockMerkleRoot(height, '0x' + computeMerkleRoot(proof).toString('hex'));
-            proof = borshifyOutcomeProof(proof);
-            expect(await NearProver.proveOutcome(proof, height)).to.be.true;
+            let proofPath = process.env['NEAR_PROOFS_DIR'] + '/' + proofFiles[i];
+            await testProof(proofPath);
             console.log('proved proof ' + proofFiles[i]);
         }
     });
 }
-
-describe('NearProver with admin access', () => {
-    const BRIDGE_ADDRESS_SLOT = 3;
-
-    beforeEach(async () => {
-        [deployerAccount] = await ethers.getSigners();
-
-        // Make the deployer admin
-        adminAccount = deployerAccount;
-
-        nearBridgeMock = await (await ethers.getContractFactory('NearBridgeMock')).deploy();
-        nearProver = await (await ethers.getContractFactory('NearProver')).deploy(
-            nearBridgeMock.address,
-            adminAccount.address,
-            0
-        );
-
-    });
-
-    describe('Upgradability', async () => {
-        it('should upgrade the bridge address', async () => {
-            const initialBridgeAddress = await nearProver.bridge();
-            expect(initialBridgeAddress)
-                .to
-                .equal(nearBridgeMock.address);
-
-            const newBridge = await (await ethers.getContractFactory('NearBridgeMock')).deploy();
-            expect(await newBridge.address)
-                .to
-                .not
-                .equal(initialBridgeAddress);
-
-            // Mask matches only on the latest 20 bytes (to store the address)
-            const mask = ethers.BigNumber.from("0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff");
-            nearProver.adminSstoreWithMask(BRIDGE_ADDRESS_SLOT, newBridge.address, mask);
-
-            expect(await nearProver.bridge())
-                .to
-                .equal(newBridge.address);
-        });
-
-        it('should upgrade the bridge address from the provided hex string', async () => {
-            const initialBridgeAddress = await nearProver.bridge();
-            expect(initialBridgeAddress)
-                .to
-                .equal(nearBridgeMock.address);
-
-            const newBridgeAddress = '0x891B2749238B27fF58e951088e55b04de71Dc374';
-            const newBridgeAddressBN = ethers.BigNumber.from(newBridgeAddress);
-
-            const mask = ethers.BigNumber.from("0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff");
-            nearProver.adminSstoreWithMask(BRIDGE_ADDRESS_SLOT, newBridgeAddressBN, mask);
-
-            expect(await nearProver.bridge())
-                .to
-                .equal(newBridgeAddress);
-        });
-    });
-});
